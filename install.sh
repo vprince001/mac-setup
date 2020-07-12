caf55bfb02dee300683086688e78829f677bd1f2
Prince Verma <princeve@Princes-MacBook-Pro.local>
2020-07-12 08:31:49 +0530

    Add installation of git
    - check if git is installed
    - ask to install if not found

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+echo "# --- HOMEBREW --- #"
 if test ! $(which brew); then
-  echo "# --- HOMEBREW --- #"
   echo "Homebrew is not installed. Would you like to install it? Press y to install else press enter"
   read -r response
   if [[ $response == "y" ]]; then
-  echo "Installing homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo "Successfully installed Homebrew"
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo "Successfully installed Homebrew"
   fi
+else
+  echo "Homebrew already installed"
 fi
 
 echo "Making sure we're using the latest Homebrew"
@@ -17,3 +19,16 @@ echo "Upgrading any already-installed formulae"
 brew upgrade
 echo "Upgrade Complete"
 echo ""
+
+echo "# --- GIT --- #"
+if test ! $(which git); then
+  echo "Git is not installed. Would you like to install it? Press y to install else press enter"
+  read -r response
+  if [[ $response == "y" ]]; then
+    echo "Installing git..."
+    brew install git
+    echo "Successfully installed git"
+  fi
+else
+  echo "Git already installed"
+fi
