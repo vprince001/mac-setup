257a6ed52a5c28ce9e713dd88c2e29f522404af4
Prince Verma <princeverma@192.168.1.9>
2022-06-05 00:49:56 +0530

    update git file with voice command and quit command

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,12 +3,16 @@ echo "
 "
 
 if test ! $(command -v git); then
-  echo "Git is not installed. Press y to install else press enter"
-  read -r response
+  echo "Git is not installed. Do you want to install Git? [y/n/q]"
+  say "Do you want to install Git?"
+  read -r -n1 response
   if [[ $response == "y" ]]; then
     echo "Installing git..."
     brew install git
     echo "Successfully installed git"
+    say "Successfully installed git"
+  elif [[ $response == "q" ]]; then
+    exit
   fi
 else
   echo "Git already installed"
