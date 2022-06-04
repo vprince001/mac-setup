9ef98f4facaadb5e0a9bb7c6e7dcb3f979550b74
Prince Verma <princeverma@192.168.1.9>
2022-06-05 02:46:31 +0530

    update vim file with voice command and quit command

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -3,12 +3,17 @@ echo "
 "
 
 if test ! $(command -v vim); then
-  echo "Vim is not installed. Press y to install else press enter"
-  read -r response
+  echo "\nVim is not installed. Do you want to install Vim? [y/n/q]"
+  say "Do you want to install Vim?"
+  read -r -n1 response
   if [[ $response == "y" ]]; then
     echo "Installing vim..."
+    say "Installing vim"
     brew install vim
-    echo "Successfully installed vim"
+    echo "Vim Successfully installed"
+    say "Vim Successfully installed"
+  elif [[ $response == "q" ]]; then
+    exit
   fi
 else
   echo "Vim already installed"
