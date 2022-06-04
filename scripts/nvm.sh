eb061111abdca22610c82f1d564f7f3c58922415
Prince Verma <princeverma@192.168.1.9>
2022-06-05 02:29:13 +0530

    update nvm file with voice command and quit command

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -3,12 +3,17 @@ echo "
 "
 
 if [[ nvm -ne 0 ]]; then
-  echo "NVM is not installed. Press y to install else press enter"
-  read -r response
+  echo "\nNVM is not installed. Do you want to install NVM? [y/n/q]"
+  say "Do you want to install NVM?"
+  read -r -n1 response
   if [[ $response == "y" ]]; then
-    echo "Installing nvm..."
+    echo "\nInstalling nvm..."
+    say "Installing nvm"
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-    echo "Successfully installed nvm"
+    echo "\nNVM Successfully installed"
+    say "\nNVM Successfully installed"
+  elif [[ $response == "q" ]]; then
+    exit
   fi
 else
   echo "nvm already installed"
